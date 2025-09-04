679a436dec7982079ecb02f54469b97361a35132
Joshua <godfrey@Joshuas-MacBook-Air.local>
2025-09-04 20:28:37 +0100

    made google font inter changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 
 import './global.css';
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
 
-const inter = Inter();
+// const inter = Inter();
 
 export default function RootLayout({
   children,
@@ -11,7 +11,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body>
+        {children}
+      </body>
+      {/* <body className={`${inter.className} antialiased`}>{children}</body> */}
     </html>
   );
 }
