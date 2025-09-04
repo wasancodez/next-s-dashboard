282c74c14422ce1f3ccfe2bbfb0e300b0f6ccd2d
Joshua <godfrey@Joshuas-MacBook-Air.local>
2025-09-04 20:15:41 +0100

    code for deployment

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+
+import './global.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter();
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
